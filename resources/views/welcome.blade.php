ee2d4022c17fa3c51edda3aaf4943094204feec4
amine mazza <aminemazza6@gmail.com>
2023-12-16 01:25:44 +0100

    add project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Dish Discover</title>
+    <link rel="shortcut icon" href="DishDiscover.jpg" type="image/x-icon">
+</head>
+<body>
+    <h1 style="text-align: center">Welcome To Dish Discover</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque quam cumque nesciunt architecto odio asperiores. Facere laboriosam cum quasi, suscipit neque sequi consequatur ipsum maxime earum cupiditate officiis odio exercitationem!</p>
+    
+</body>
+</html>
